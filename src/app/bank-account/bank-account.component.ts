f5ded1665105abd49be8b60ae25c56a8ff675267
Kertsu <kurtddbigtas@gmail.com>
2023-07-25 17:09:31 +0800

    added bank account screen

---
+++ src/app/bank-account/bank-account.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { LABELS } from '../core/models/labels';
+
+@Component({
+  selector: 'app-bank-account',
+  templateUrl: './bank-account.component.html',
+  styleUrls: ['./bank-account.component.css'],
+})
+export class BankAccountComponent {
+  labels = LABELS['bank'];
+}
