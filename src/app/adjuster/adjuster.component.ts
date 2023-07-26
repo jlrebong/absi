b43e4ba88ce635f20f7380492eb5359803be1348
unknown <jrebong0@gmail.com>
2023-07-26 11:43:33 +0800

    move setter and getter into the model

--- src/app/adjuster/adjuster.component.ts
+++ src/app/adjuster/adjuster.component.ts
@@ -21,37 +21,11 @@ export class AdjusterComponent {
     this.adjuster.adjusterStatus = "new";
   }
 
-  get information () {
-    return {
-      status: this.adjuster.adjusterStatus || 'new',
-      name: this.adjuster.adjusterName  || '',
-      code: this.adjuster.adjusterCode  || '',
-      specialty: this.adjuster.adjusterSpecialty  || ''
-    }
-  }
-
   setInformation(e) {
-    this.adjuster.adjusterStatus    = e.status;
-    this.adjuster.adjusterName      = e.name;
-    this.adjuster.adjusterCode      = e.code;
-    this.adjuster.adjusterSpecialty = e.specialty;
-  }
-
-  get location () {
-    return {
-      address: this.adjuster.adjusterAddress || '',
-      country: this.adjuster.adjusterCountry  || '',
-      province: this.adjuster.adjusterProvince  || '',
-      city: this.adjuster.adjusterCity  || '',
-      zip: this.adjuster.adjusterZipcode  || ''
-    }
+    this.adjuster.information = e;
   }
 
   setLocation(e) {
-    this.adjuster.adjusterAddress  = e.address;
-    this.adjuster.adjusterCountry  = e.country;
-    this.adjuster.adjusterProvince = e.province;
-    this.adjuster.adjusterCity     = e.city;
-    this.adjuster.adjusterZipcode  = e.zip;
+    this.adjuster.location = e;
   }
 }
--- src/app/core/models/adjuster.ts
+++ src/app/core/models/adjuster.ts
@@ -4,6 +4,40 @@ import { ContactPhone } from "./contact-phone"
 // interface-class merging
 export interface AdjusterClass extends Adjuster {}
 export class AdjusterClass {
+
+    get information () {
+        return {
+            status: this.adjusterStatus || 'new',
+            name: this.adjusterName  || '',
+            code: this.adjusterCode  || '',
+            specialty: this.adjusterSpecialty  || ''
+        }
+    }
+
+    set information(e) {
+        this.adjusterStatus    = e.status;
+        this.adjusterName      = e.name;
+        this.adjusterCode      = e.code;
+        this.adjusterSpecialty = e.specialty;
+    }
+
+    get location () {
+        return {
+            address: this.adjusterAddress || '',
+            country: this.adjusterCountry  || '',
+            province: this.adjusterProvince  || '',
+            city: this.adjusterCity  || '',
+            zip: this.adjusterZipcode  || ''
+        }
+    }
+
+    set location(e) {
+        this.adjusterAddress  = e.address;
+        this.adjusterCountry  = e.country;
+        this.adjusterProvince = e.province;
+        this.adjusterCity     = e.city;
+        this.adjusterZipcode  = e.zip;
+    }
 }
 
 export interface Adjuster {
